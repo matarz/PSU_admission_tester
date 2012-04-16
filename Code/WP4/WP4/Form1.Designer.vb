55cc249461bb3c863b94b6c966eca2113f29b854
matarz <matarz745@gmail.com>
2012-04-16 16:15:49 +0300

    adding code and license files.

---
+++ Code/WP4/WP4/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NameBox = New System.Windows.Forms.TextBox()
+        Me.YearOfGraduationBox = New System.Windows.Forms.TextBox()
+        Me.GPABox = New System.Windows.Forms.TextBox()
+        Me.EnglishBox = New System.Windows.Forms.TextBox()
+        Me.MathBox = New System.Windows.Forms.TextBox()
+        Me.ScienceBox = New System.Windows.Forms.TextBox()
+        Me.SocialStudiesBox = New System.Windows.Forms.TextBox()
+        Me.LanguageBox = New System.Windows.Forms.TextBox()
+        Me.SATBox = New System.Windows.Forms.TextBox()
+        Me.ACTBox = New System.Windows.Forms.TextBox()
+        Me.DecisionBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.CheckButton = New System.Windows.Forms.Button()
+        Me.DoneButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'NameBox
+        '
+        Me.NameBox.Location = New System.Drawing.Point(89, 34)
+        Me.NameBox.Name = "NameBox"
+        Me.NameBox.Size = New System.Drawing.Size(273, 20)
+        Me.NameBox.TabIndex = 0
+        '
+        'YearOfGraduationBox
+        '
+        Me.YearOfGraduationBox.Location = New System.Drawing.Point(120, 75)
+        Me.YearOfGraduationBox.Name = "YearOfGraduationBox"
+        Me.YearOfGraduationBox.Size = New System.Drawing.Size(100, 20)
+        Me.YearOfGraduationBox.TabIndex = 1
+        '
+        'GPABox
+        '
+        Me.GPABox.Location = New System.Drawing.Point(296, 75)
+        Me.GPABox.Name = "GPABox"
+        Me.GPABox.Size = New System.Drawing.Size(66, 20)
+        Me.GPABox.TabIndex = 2
+        '
+        'EnglishBox
+        '
+        Me.EnglishBox.Location = New System.Drawing.Point(178, 117)
+        Me.EnglishBox.Name = "EnglishBox"
+        Me.EnglishBox.Size = New System.Drawing.Size(100, 20)
+        Me.EnglishBox.TabIndex = 3
+        '
+        'MathBox
+        '
+        Me.MathBox.Location = New System.Drawing.Point(178, 143)
+        Me.MathBox.Name = "MathBox"
+        Me.MathBox.Size = New System.Drawing.Size(100, 20)
+        Me.MathBox.TabIndex = 4
+        '
+        'ScienceBox
+        '
+        Me.ScienceBox.Location = New System.Drawing.Point(178, 169)
+        Me.ScienceBox.Name = "ScienceBox"
+        Me.ScienceBox.Size = New System.Drawing.Size(100, 20)
+        Me.ScienceBox.TabIndex = 5
+        '
+        'SocialStudiesBox
+        '
+        Me.SocialStudiesBox.Location = New System.Drawing.Point(178, 195)
+        Me.SocialStudiesBox.Name = "SocialStudiesBox"
+        Me.SocialStudiesBox.Size = New System.Drawing.Size(100, 20)
+        Me.SocialStudiesBox.TabIndex = 6
+        '
+        'LanguageBox
+        '
+        Me.LanguageBox.Location = New System.Drawing.Point(178, 221)
+        Me.LanguageBox.Name = "LanguageBox"
+        Me.LanguageBox.Size = New System.Drawing.Size(100, 20)
+        Me.LanguageBox.TabIndex = 7
+        '
+        'SATBox
+        '
+        Me.SATBox.Location = New System.Drawing.Point(178, 261)
+        Me.SATBox.Name = "SATBox"
+        Me.SATBox.Size = New System.Drawing.Size(100, 20)
+        Me.SATBox.TabIndex = 8
+        '
+        'ACTBox
+        '
+        Me.ACTBox.Location = New System.Drawing.Point(178, 287)
+        Me.ACTBox.Name = "ACTBox"
+        Me.ACTBox.Size = New System.Drawing.Size(100, 20)
+        Me.ACTBox.TabIndex = 9
+        '
+        'DecisionBox
+        '
+        Me.DecisionBox.Location = New System.Drawing.Point(75, 375)
+        Me.DecisionBox.Name = "DecisionBox"
+        Me.DecisionBox.ReadOnly = True
+        Me.DecisionBox.Size = New System.Drawing.Size(100, 20)
+        Me.DecisionBox.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Year of Graduation"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(251, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "GPA"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Units of English"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 146)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(70, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Units of Math"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 172)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Units of Science"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 198)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(113, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Units of Social Studies"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 224)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(134, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Units of Second Language"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 264)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(59, 13)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "SAT Score"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 290)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(59, 13)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "ACT Score"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(12, 378)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(48, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "Decision"
+        '
+        'CheckButton
+        '
+        Me.CheckButton.Location = New System.Drawing.Point(191, 313)
+        Me.CheckButton.Name = "CheckButton"
+        Me.CheckButton.Size = New System.Drawing.Size(75, 23)
+        Me.CheckButton.TabIndex = 22
+        Me.CheckButton.Text = "Check"
+        Me.CheckButton.UseVisualStyleBackColor = True
+        '
+        'DoneButton
+        '
+        Me.DoneButton.Location = New System.Drawing.Point(289, 350)
+        Me.DoneButton.Name = "DoneButton"
+        Me.DoneButton.Size = New System.Drawing.Size(75, 45)
+        Me.DoneButton.TabIndex = 23
+        Me.DoneButton.Text = "I'm Done"
+        Me.DoneButton.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AcceptButton = Me.CheckButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 406)
+        Me.Controls.Add(Me.DoneButton)
+        Me.Controls.Add(Me.CheckButton)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DecisionBox)
+        Me.Controls.Add(Me.ACTBox)
+        Me.Controls.Add(Me.SATBox)
+        Me.Controls.Add(Me.LanguageBox)
+        Me.Controls.Add(Me.SocialStudiesBox)
+        Me.Controls.Add(Me.ScienceBox)
+        Me.Controls.Add(Me.MathBox)
+        Me.Controls.Add(Me.EnglishBox)
+        Me.Controls.Add(Me.GPABox)
+        Me.Controls.Add(Me.YearOfGraduationBox)
+        Me.Controls.Add(Me.NameBox)
+        Me.KeyPreview = True
+        Me.Name = "Main"
+        Me.Text = "Admission Check for High School Graduates"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NameBox As System.Windows.Forms.TextBox
+    Friend WithEvents YearOfGraduationBox As System.Windows.Forms.TextBox
+    Friend WithEvents GPABox As System.Windows.Forms.TextBox
+    Friend WithEvents EnglishBox As System.Windows.Forms.TextBox
+    Friend WithEvents MathBox As System.Windows.Forms.TextBox
+    Friend WithEvents ScienceBox As System.Windows.Forms.TextBox
+    Friend WithEvents SocialStudiesBox As System.Windows.Forms.TextBox
+    Friend WithEvents LanguageBox As System.Windows.Forms.TextBox
+    Friend WithEvents SATBox As System.Windows.Forms.TextBox
+    Friend WithEvents ACTBox As System.Windows.Forms.TextBox
+    Friend WithEvents DecisionBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents CheckButton As System.Windows.Forms.Button
+    Friend WithEvents DoneButton As System.Windows.Forms.Button
+
+End Class
---
+++ Code/WP4/WP4/Form1.vb
@@ -0,0 +1,105 @@
+﻿Public Class Main
+    Dim Lines(,) As Integer = {{19, 0}, {21, 0}, {27, 0}, {29, 0}, {35, 0}, {37, 0}, {39, 0}, {44, 0}, {46, 0}, {48, 0}, {54, 0}, {60, 0}, {62, 0}, {67, 0}, {72, 0}, {73, 0}, {78, 0}, {79, 0}, {84, 0}}
+
+    Dim CurrentYear As Integer = 2011       'to calculate time since graduation
+
+    'array to hold GPA values under 3
+    Dim Under3GPA() As String = {2.99, 2.98, 2.97, 2.96, 2.95, 2.94, 2.93, 2.92, 2.91, 2.9, 2.89, 2.88, 2.87, 2.86, 2.85, 2.84, 2.83, 2.82, 2.81, 2.8, 2.79, 2.78, 2.77, 2.76, 2.75, 2.74, 2.73, 2.72, 2.71, 2.7}
+
+    'array to hold SAT values for GPA values under 3
+    Dim Under3GPA_SAT() As String = {1060, 1070, 1080, 1080, 1090, 1100, 1110, 1120, 1120, 1130, 1140, 1150, 1160, 1160, 1170, 1180, 1190, 1200, 1200, 1210, 1220, 1230, 1230, 1240, 1250, 1260, 1270, 1280, 1280, 1290}
+
+    'array to hold ACT values for GPA values under 3
+    Dim Under3GPA_ACT() As String = {23, 23, 23, 23, 24, 24, 24, 24, 24, 25, 25, 25, 25, 25, 26, 26, 26, 26, 26, 27, 27, 27, 27, 27, 28, 28, 28, 28, 28, 29}
+
+    Private Sub CheckButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckButton.Click
+
+        'Check if year, gpa, and units are valid inputs
+        Lines(0, 1) = 1 'LC
+        If Not PosInt(YearOfGraduationBox.Text) Or Not PosInt(GPABox.Text) Or Not PosInt(EnglishBox.Text) Or Not PosInt(MathBox.Text) Or Not PosInt(ScienceBox.Text) Or Not PosInt(SocialStudiesBox.Text) Or Not PosInt(LanguageBox.Text) Then
+            Lines(1, 1) = 1 'LC
+            MsgBox("Field value not valid")
+            Exit Sub
+        End If
+
+        'check gpa and units for accepted rates
+        Lines(2, 1) = 1 'LC
+        If CInt(GPABox.Text) < 2.7 Or CInt(EnglishBox.Text) < 4 Or CInt(MathBox.Text) < 3 Or CInt(ScienceBox.Text) < 3 Or CInt(SocialStudiesBox.Text) < 3 Or CInt(LanguageBox.Text) < 2 Then
+            Lines(3, 1) = 1 'LC
+            DecisionBox.Text = "Denied"
+            Exit Sub
+        End If
+
+        'if gpa is less than 3 but more than 2.69 then apply gpa & SAT & ACT calculations
+        Lines(4, 1) = 1 'LC
+        If GPABox.Text < 3 Then
+            Lines(5, 1) = 1 'LC
+            If Not PosInt(SATBox.Text) Or Not PosInt(ACTBox.Text) Then
+                Lines(6, 1) = 1 'LC
+                MsgBox("You need a SAT and ACT score with this GPA")
+                Exit Sub
+            End If
+
+            Lines(7, 1) = 1 'LC
+            For i = 0 To Under3GPA.Length - 1 Step 1
+                Lines(8, 1) = 1 'LC
+                If CDbl(GPABox.Text) = Under3GPA(i) And CInt(SATBox.Text) >= Under3GPA_SAT(i) And CInt(ACTBox.Text) >= Under3GPA_ACT(i) Then
+                    Lines(9, 1) = 1 'LC
+                    DecisionBox.Text = "Accepted"
+                    Exit Sub
+                End If
+            Next
+
+            Lines(10, 1) = 1 'LC
+            DecisionBox.Text = "Denied"
+            Exit Sub
+        End If
+
+        'if gpa is more than 3
+        Lines(11, 1) = 1 'LC
+        If CurrentYear - CInt(YearOfGraduationBox.Text) < 3 And (Not PosInt(SATBox.Text) Or Not PosInt(ACTBox.Text)) Then
+            Lines(12, 1) = 1 'LC
+            MsgBox("You are required to provide SAT and ACT Scores")
+            Exit Sub
+        End If
+
+        Lines(13, 1) = 1 'LC
+        DecisionBox.Text = "Accepted"
+    End Sub
+
+    Private Function PosInt(ByVal Number As String) As Boolean
+        Lines(14, 1) = 1 'LC
+        If Number.Length < 1 Then
+            Lines(15, 1) = 1 'LC
+            Return False
+        End If
+
+        Lines(16, 1) = 1 'LC
+        If Not IsNumeric(Number) Or Number < 0 Then
+            Lines(17, 1) = 1 'LC
+            Return False
+        End If
+
+        Lines(18, 1) = 1 'LC
+        Return True
+    End Function
+
+    Private Sub DoneButton_Click(sender As System.Object, e As System.EventArgs) Handles DoneButton.Click
+        Dim LinesExecuted = 0
+        Dim LinesNotExecuted = ""
+
+        For i = 0 To Lines.GetLength(0) - 1 Step 1
+            If Lines(i, 1) = 1 Then
+                LinesExecuted += 1
+            Else
+                LinesNotExecuted += Lines(i, 0).ToString() + ", "
+            End If
+        Next
+
+        Dim Coverage As Double = Math.Round(100 * (LinesExecuted / Lines.GetLength(0)), 2)
+        If LinesNotExecuted.Length > 2 Then
+            LinesNotExecuted = LinesNotExecuted.Remove(LinesNotExecuted.Length - 2)
+        End If
+        MsgBox("Coverage: " + Coverage.ToString() + "%" + Environment.NewLine + "Lines not exercised: " + LinesNotExecuted)
+    End Sub
+End Class
